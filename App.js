5ec00ec76feca220cab5a83c63a3b5451680fa3a
David Anderson 6 <davidandersonsix@gmail.com>
2020-04-26 10:06:17 -0400

    Added lottie animation to main screen to display it is working. Reworked layout of main screen

--- App.js
+++ App.js
@@ -9,88 +9,40 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
-import {
-  AdMobBanner,
-  AdMobInterstitial,
-  PublisherBanner,
-  AdMobRewarded
-} from 'react-native-admob'
+import LottieView from 'lottie-react-native';
+import { AdMobBanner } from 'react-native-admob'
 
 class App extends React.Component {
   render() {
     return (
       <>
         <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <View style={{ borderWidth: 1 }}>
+        <SafeAreaView style={{ borderWidth: 5, borderColor: 'green', flex: 1 }}>
+          <View style={{ borderWidth: 1, justifyContent: 'center', alignItems: 'center', flex: 1, width: '100%' }}>
             <Text>
-            Dave</Text>
-          <AdMobBanner
-            adSize='banner'
-            adUnitID='ca-app-pub-3940256099942544/6300978111'
-            testDevices={[AdMobBanner.simulatorId]}
-            onDidFailToReceiveAdWithError={() => console.log('no ad')}
-          />
+              Lottie + AdMob + Noti Test
+            </Text>
+
+            <View style={{ width: 200, height: 200, borderWidth: 1, borderColor: 'red' }}>
+              <LottieView source={require('./images/doggieTrot.json')} autoPlay loop />
+            </View>
+
+            <AdMobBanner
+              adSize='banner'
+              adUnitID='ca-app-pub-3940256099942544/6300978111'
+              testDevices={[AdMobBanner.simulatorId]}
+              onDidFailToReceiveAdWithError={() => console.log('no ad')}
+              style={{ position: 'absolute', bottom: 0 }}
+            />
           </View>
         </SafeAreaView>
       </>
     );
   }
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
